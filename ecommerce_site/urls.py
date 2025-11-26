614111feaff945db9b5b2a54ae7370f69c140887
harishsathwick <harishjakkamsetty76@gmail.com>
2025-11-26 13:04:53 +0530

    Enable media serving on Render

--- ecommerce_site/urls.py
+++ ecommerce_site/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', include('shop.urls')),
 ]
 
+# Serve media files (product images, avatars) on Render too
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
